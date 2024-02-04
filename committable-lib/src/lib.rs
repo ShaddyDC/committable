6d9539d91e793da0ae5bd7dd2c303790c1ed510d
ShaddyDC <shaddythefirst@gmail.com>
2024-02-04 16:14:09 +0100

    feat: Add simple rules

--- committable-lib/src/lib.rs
+++ committable-lib/src/lib.rs
@@ -11,7 +11,7 @@ impl<'a> Commit<'a> {
         self.commit_string
     }
 
-    pub fn get_title(&self) -> &'a str {
+    pub fn get_header(&self) -> &'a str {
         self.commit_string.lines().next().unwrap_or("")
     }
 
@@ -36,6 +36,60 @@ impl<'a> Commit<'a> {
     }
 }
 
+trait Rule {
+    fn check(&self, commit: &Commit) -> Result<(), ()>;
+}
+
+struct NonEmptyHeader {}
+impl Rule for NonEmptyHeader {
+    fn check(&self, commit: &Commit) -> Result<(), ()> {
+        if commit.get_header().is_empty() {
+            Err(())
+        } else {
+            Ok(())
+        }
+    }
+}
+
+struct NonEmptyBody {}
+impl Rule for NonEmptyBody {
+    fn check(&self, commit: &Commit) -> Result<(), ()> {
+        let content = commit.commit_string;
+        let Some((_, rest)) = content.split_once('\n') else {
+            return Ok(());
+        };
+
+        if !rest.is_empty() && commit.get_body().is_none() {
+            Err(())
+        } else {
+            Ok(())
+        }
+    }
+}
+
+struct SingleEmptyLineBeforeBody {}
+
+impl Rule for SingleEmptyLineBeforeBody {
+    fn check(&self, commit: &Commit) -> Result<(), ()> {
+        let content = commit.commit_string;
+        let Some((_, rest)) = content.split_once('\n') else {
+            return Ok(());
+        };
+
+        if !rest.is_empty() && !rest.starts_with('\n') && !rest.starts_with("\r\n") {
+            return Err(());
+        }
+
+        let (_, rest) = rest.split_once('\n').unwrap_or(("", rest));
+
+        if rest.starts_with('\n') || rest.starts_with("\r\n") {
+            return Err(());
+        }
+
+        Ok(())
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -44,7 +98,7 @@ mod tests {
     fn only_header() {
         let c = Commit::new("Hello world");
         assert_eq!(c.get_commit_string(), "Hello world");
-        assert_eq!(c.get_title(), "Hello world");
+        assert_eq!(c.get_header(), "Hello world");
         assert_eq!(c.get_body(), None);
     }
 
@@ -52,7 +106,7 @@ mod tests {
     fn empty_body() {
         let c = Commit::new("Hello world\n\n");
         assert_eq!(c.get_commit_string(), "Hello world\n\n");
-        assert_eq!(c.get_title(), "Hello world");
+        assert_eq!(c.get_header(), "Hello world");
         assert_eq!(c.get_body(), None);
     }
 
@@ -60,7 +114,7 @@ mod tests {
     fn some_body() {
         let c = Commit::new("Hello world\nSome Body\n");
         assert_eq!(c.get_commit_string(), "Hello world\nSome Body\n");
-        assert_eq!(c.get_title(), "Hello world");
+        assert_eq!(c.get_header(), "Hello world");
         assert_eq!(c.get_body(), Some("Some Body\n"));
     }
 
@@ -68,7 +122,41 @@ mod tests {
     fn more_body() {
         let c = Commit::new("Hello world\n\nSome Body");
         assert_eq!(c.get_commit_string(), "Hello world\n\nSome Body");
-        assert_eq!(c.get_title(), "Hello world");
+        assert_eq!(c.get_header(), "Hello world");
         assert_eq!(c.get_body(), Some("Some Body"));
+    }
+
+    #[test]
+    fn nonempty_header() {
+        let c = NonEmptyHeader {};
+        assert_eq!(c.check(&Commit::new("")), Err(()));
+        assert_eq!(c.check(&Commit::new("\nTest")), Err(()));
+        assert_eq!(c.check(&Commit::new("\n\nTest")), Err(()));
+        assert_eq!(c.check(&Commit::new("YEP\n\nTest")), Ok(()));
+        assert_eq!(c.check(&Commit::new("YEP")), Ok(()));
+    }
+
+    #[test]
+    fn nonempty_body() {
+        let c = NonEmptyBody {};
+        assert_eq!(c.check(&Commit::new("H")), Ok(()));
+        assert_eq!(c.check(&Commit::new("H\n")), Ok(()));
+        assert_eq!(c.check(&Commit::new("H\n\nTest")), Ok(()));
+        assert_eq!(c.check(&Commit::new("H\n\n\nTest")), Ok(()));
+        assert_eq!(c.check(&Commit::new("H\nYEP\n\nTest")), Ok(()));
+        assert_eq!(c.check(&Commit::new("H\nYEP")), Ok(()));
+        assert_eq!(c.check(&Commit::new("H\n\n")), Err(()));
+    }
+
+    #[test]
+    fn single_empty_line_before_body() {
+        let c = SingleEmptyLineBeforeBody {};
+        assert_eq!(c.check(&Commit::new("H")), Ok(()));
+        assert_eq!(c.check(&Commit::new("H\n")), Ok(()));
+        assert_eq!(c.check(&Commit::new("H\n\nTest")), Ok(()));
+        assert_eq!(c.check(&Commit::new("H\n\n\nTest")), Err(()));
+        assert_eq!(c.check(&Commit::new("H\nYEP\n\nTest")), Err(()));
+        assert_eq!(c.check(&Commit::new("H\nYEP")), Err(()));
+        assert_eq!(c.check(&Commit::new("H\n\nNice!\n\n")), Ok(()));
     }
 }
